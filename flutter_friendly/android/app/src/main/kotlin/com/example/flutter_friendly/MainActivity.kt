03107f36ebc94294353e98f115f205c588932e65
m1_air <peterjbishop.denver@gmail.com>
2025-01-29 11:51:49 -0700

    flutter go project files

---
+++ flutter_friendly/android/app/src/main/kotlin/com/example/flutter_friendly/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_friendly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ go_winner/go.mod
@@ -0,0 +1,3 @@
+module friendly-winner/go_winner/main.go
+
+go 1.23.5
